1a30620098553660ee81de1313d1e79b0b1d0c89
Manikandan Sundararajan <mani@hey.com>
2023-03-18 03:34:59 -0400

    update test fns

--- tests/api_key.rs
+++ tests/api_key.rs
@@ -1,4 +1,4 @@
-use openai_rust::{self, open_ai_api};
+use openai_rust;
 
 mod common;
 
@@ -7,9 +7,9 @@ fn test_config_org_and_key_validation() {
     let org = String::from("orgstring");
     let key = String::from("keystring");
 
-    let client = open_ai_api::Client::new_with_org(key, org);
-    assert_eq!(client.config.key, String::from("keystring"));
-    assert_eq!(client.config.organization, Some(String::from("orgstring")));
+    let client = openai_rust::Client::new_with_org(key, org);
+    assert_eq!(client.config.openai_secret_key, String::from("keystring"));
+    assert_eq!(client.config.openai_org, Some(String::from("orgstring")));
 }
 
 #[test]
@@ -17,16 +17,16 @@ fn test_config_from_env() {
     common::setup();
     let key = std::env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY must be set");
 
-    let client = open_ai_api::Client::new(key);
-    assert_ne!(client.config.key.len(), 0);
+    let client = openai_rust::Client::new(key);
+    assert_ne!(client.config.openai_secret_key.len(), 0);
 }
 
 #[test]
-fn test_authenticate() {
+fn test_models() {
     common::setup();
 
     let key = std::env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY must be set");
-    let client = open_ai_api::Client::new(key);
+    let client = openai_rust::Client::new(key);
 
-    println!("{:?}", client.authenticate().unwrap());
+    println!("{:?}", client.get_models().unwrap());
 }
