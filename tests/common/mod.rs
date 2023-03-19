47080013442585b30d24995761c80c964745b4bb
Manikandan Sundararajan <mani@hey.com>
2023-03-19 04:16:38 -0400

    setup a full working client in common setup

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,5 +1,11 @@
+use crate::openai_rust;
 use dotenv::dotenv;
 
-pub fn setup() {
-    dotenv().ok(); // This line loads the environment variables from the ".env" file.
+pub fn setup() -> openai_rust::Client {
+    // This line loads the environment variables from the ".env" file.
+    dotenv().ok();
+
+    // Construct a simple client using the key
+    let key = std::env::var("OPENAI_API_KEY").expect("OPENAI_API_KEY must be set");
+    openai_rust::Client::new(key)
 }
